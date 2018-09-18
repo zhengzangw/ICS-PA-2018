0317cc8093355dc538c71037b9d712430eef79c6
tracer-ics2018 <tracer@njuics.org>
2018-09-18 00:44:47 -0400

    >  compile 171860658 fermat Linux bad 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  00:44:47 up  4:30,  5 users,  load average: 0.10, 0.12, 0.08 88c5fea924c69d43b5c2af3e7ede350527d13128

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -88,7 +88,7 @@ static bool make_token(char *e) {
 		
         switch (rules[i].token_type) {
 			case TK_NUM:
-				assert(substr_len<32); strncmp(tokens[nr_token].str,substr_start,substr_len);
+				assert(substr_len<32); strncpy(tokens[nr_token].str,substr_start,substr_len);
           default: break;
         }
 		tokens[nr_token++].type = rules[i].token_type;
