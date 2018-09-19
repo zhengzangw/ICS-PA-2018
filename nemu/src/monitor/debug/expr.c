cbcef4a5dc862194165def15cff8bb75ab85c8fb
tracer-ics2018 <tracer@njuics.org>
2018-09-19 05:13:53 -0400

    >  compile 171860658 fermat Linux bad 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  05:13:53 up 1 day,  2:27,  4 users,  load average: 0.14, 0.17, 0.18 c6eed2de441d89d410499aa1c48b74ade03ad052

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -184,7 +184,7 @@ uint32_t eval(int s, int t, bool *success, char *e){
 			case '-': return val1-val2;
 			case '*': return val1*val2;
 			case '/': if (val2==0) {
-						printf("Divided by zero at position %d\n%s\n%*.s^\n",op,e,op,"");
+						printf("Divided by zero!\n");
 						*success = false;
 						return 0;
 					  }
