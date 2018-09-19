09db2007d05ab7a854b6e2fe18be5bd0d7961f37
tracer-ics2018 <tracer@njuics.org>
2018-09-18 22:25:51 -0400

    >  compile 171860658 fermat Linux bad 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  22:25:51 up 20:50,  4 users,  load average: 0.16, 0.12, 0.07 c062c728ea3b04f4b271055d74bf574f6d48e3b5

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -92,10 +92,10 @@ static bool make_token(char *e) {
 					printf("Input a number too larger!\n");
 					return false;
 				}	
-				uint32_t val = 0,len = strlen(tokens[nr_token].str);
-				Log("%d",len);
-				for (int i=0;i<len;++i)
-					val = val*10+tokens[nr_token].str[len-i-1]-'0';
+				uint32_t val = 0;
+				Log("%d",substr_len);
+				for (int i=0;i<substr_len;++i)
+					val = val*10+tokens[nr_token].str[substr_len-i-1]-'0';
 				tokens[nr_token++].type = rules[i].token_type;
 				break;
 			case TK_NOTYPE:
