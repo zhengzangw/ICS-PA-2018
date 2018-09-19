6d0c0c045d93c1e228b0f1b87160cde694d6a007
tracer-ics2018 <tracer@njuics.org>
2018-09-19 03:06:02 -0400

    >  compile 171860658 fermat Linux bad 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  03:06:02 up 1 day, 23 min,  4 users,  load average: 0.02, 0.05, 0.02 a1cf35506566e634c1fbc0f26e8a7b36f155969f

--- nemu/tools/gen-expr/gen-expr.c
+++ nemu/tools/gen-expr/gen-expr.c
@@ -5,10 +5,38 @@
 #include <assert.h>
 #include <string.h>
 
+#define choose(x) (rand()%x)
 // this should be enough
 static char buf[65536];
+static inline void gen_rand_op(){
+   switch (choose(4)){
+	    case 0: strcat(buf,"+"); break;
+		case 1: strcat(buf,"-"); break;
+		case 2: strcat(buf,"*"); break;
+		default: strcat(buf,"/");
+   }
+}
+
+static inline void gen_rand_num(){
+	char tmp[32];
+   	sprintf(tmp, "%u", rand());
+	switch(choose(2)){
+		case 0:
+		   	strcat(buf, tmp);
+			break;
+		default:   	
+			strcat(buf, "(-");
+			strcat(buf, tmp);
+			strcat(buf,")");
+	}
+}
+
 static inline void gen_rand_expr() {
-  buf[0] = '\0';
+	switch (choose(3)){
+		case 0: gen_rand_num(); break;
+		case 1: strcat(buf,"("); gen_rand_num(); strcat(buf,")"); break;
+		default: gen_rand_expr(); gen_rand_op(); gen_rand_expr(); break;
+	}
 }
 
 static char code_buf[65536];
@@ -29,8 +57,9 @@ int main(int argc, char *argv[]) {
   }
   int i;
   for (i = 0; i < loop; i ++) {
-    gen_rand_expr();
-
+    buf[0] = '\0';
+	gen_rand_expr();
+	
     sprintf(code_buf, code_format, buf);
 
     FILE *fp = fopen(".code.c", "w");
