c94c792a766cd4e76bf9db0b7b5372ca38273f70
tracer-ics2018 <tracer@njuics.org>
2018-09-18 23:31:24 -0400

    >  compile 171860658 fermat Linux bad 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  23:31:24 up 21:56,  4 users,  load average: 0.05, 0.17, 0.12 8e4d9896b9e2f59e6c1942f97e5c4fd17e97b1

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -95,6 +95,7 @@ static bool make_token(char *e) {
     }
 
     if (i == NR_REGEX) {
+	  Log("%c",e[position]);
       printf("no match at position %d\n%s\n%*.s^\n", position, e, position, "");
 	  assert(0);
       return false;
