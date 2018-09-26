076e8539cb3382acbf8e17ff4b090ff7a8e07517
tracer-ics2018 <tracer@njuics.org>
2018-09-26 12:08:50 -0400

    >  compile 171860658 fermat Linux bad 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  12:08:50 up 4 days,  3:42,  9 users,  load average: 0.03, 0.06, 0.07 c477950d7c39cbd76b7aa128ed0ec25cb18e09c0

--- nemu/src/cpu/decode/decode.c
+++ nemu/src/cpu/decode/decode.c
@@ -327,7 +327,7 @@ make_DHelper(out_a2dx) {
 
 make_DHelper(call){
   decode_op_I(eip, id_src, true); 
-  decoding.jmp_eip = id_src->val;
+  decoding.jmp_eip = *eip + id_src->val;
 }
 
 void operand_write(Operand *op, rtlreg_t* src) {
