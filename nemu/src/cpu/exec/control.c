0319a34b87023fd83a6ccd0d0a86f204a9b6323f
tracer-ics2018 <tracer@njuics.org>
2018-09-26 13:16:55 -0400

    >  compile 171860658 fermat Linux bad 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  13:16:55 up 4 days,  4:50,  9 users,  load average: 0.16, 0.09, 0.04 16ab784d28dddee7603fed007981c0fe56db4a75

--- nemu/src/cpu/exec/control.c
+++ nemu/src/cpu/exec/control.c
@@ -27,7 +27,7 @@ make_EHelper(jmp_rm) {
 make_EHelper(call) {
   // the target address is calculated at the decode stage
   rtl_j(decoding.jmp_eip);
-  printf("\033[;6mexec_call\n\033[0m");
+  Log("call");
 
   print_asm("call %x", decoding.jmp_eip);
 }
