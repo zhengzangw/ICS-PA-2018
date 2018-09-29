14c40a9c6b3c0a65a8ea96d523045862723c1070
tracer-ics2018 <tracer@njuics.org>
2018-09-29 06:37:39 -0400

    >  compile 171860658 fermat Linux bad 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  06:37:39 up 4 days, 18:14,  7 users,  load average: 0.36, 0.19, 0.12 5c12277f804ff8d68dc40a7f8251d688170f3af6

--- nemu/src/cpu/exec/exec.c
+++ nemu/src/cpu/exec/exec.c
@@ -234,7 +234,7 @@ void exec_wrapper(bool print_flag) {
 
   decoding.seq_eip = ori_eip;
   exec_real(&decoding.seq_eip);
-
+  Log("ori=%u, seq_eip=%u, eip=%u", ori_eip, decoding.seq_eip, cpu.eip);
 #ifdef DEBUG
   int instr_len = decoding.seq_eip - ori_eip;
   sprintf(decoding.p, "%*.s", 50 - (12 + 3 * instr_len), "");
