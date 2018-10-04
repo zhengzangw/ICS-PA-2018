a6d65a48c2806bee1c692723991696351c62093e
tracer-ics2018 <tracer@njuics.org>
2018-10-05 00:24:16 +0800

    >  compile 171860658 fermat Linux bad 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  00:24:16 up 6 days,  4:18, 10 users,  load average: 0.12, 0.13, 0.10 dd73cf612bd78e101f0afef0663bd0b7263f7972

--- nemu/src/cpu/decode/modrm.c
+++ nemu/src/cpu/decode/modrm.c
@@ -93,7 +93,7 @@ void read_ModR_M(vaddr_t *eip, Operand *rm, bool load_rm_val, Operand *reg, bool
     snprintf(reg->str, OP_STR_SIZE, "%%%s", reg_name(reg->reg, reg->width));
 #endif
   }
-
+Log("Hi");
   if (m.mod == 3) {
     rm->type = OP_TYPE_REG;
     rm->reg = m.R_M;
