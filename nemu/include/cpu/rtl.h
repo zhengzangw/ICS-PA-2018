0b35d041ee2232b819719a15666a141ce2d9dcf0
171860658-Zheng Zangwei <zzw@smail.nju.edu.cn>
2018-10-05 19:53:25 +0800

    feat(rtl): implement not use -1
    not test

--- nemu/include/cpu/rtl.h
+++ nemu/include/cpu/rtl.h
@@ -150,7 +150,7 @@ static inline void rtl_sr(int r, const rtlreg_t* src1, int width) {
 
 static inline void rtl_not(rtlreg_t *dest, const rtlreg_t* src1) {
   // dest <- ~src1
-	TODO();
+  rtl_xori(dest, src1, -1);
 }
 
 static inline void rtl_sext(rtlreg_t* dest, const rtlreg_t* src1, int width) {
