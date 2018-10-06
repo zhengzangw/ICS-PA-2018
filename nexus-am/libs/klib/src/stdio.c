c6701397d34f6a2e2ffa74c717993e721d3edfc2
tracer-ics2018 <tracer@njuics.org>
2018-10-06 16:35:35 +0800

    >  run 171860658 fermat Linux bad 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  16:35:35 up 7 days,  4:20, 10 users,  load average: 0.01, 0.06, 0.08 48bdc9a55577eb62739f2c36429d40a915870889

--- nexus-am/libs/klib/src/stdio.c
+++ nexus-am/libs/klib/src/stdio.c
@@ -4,18 +4,22 @@
 #ifndef __ISA_NATIVE__
 
 int printf(const char *fmt, ...) {
+  TODO();
   return 0;
 }
 
 int vsprintf(char *out, const char *fmt, va_list ap) {
+  TODO();
   return 0;
 }
 
 int sprintf(char *out, const char *fmt, ...) {
+  TODO();
   return 0;
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
+  TODO();
   return 0;
 }
 
--- nexus-am/libs/klib/src/string.c
+++ nexus-am/libs/klib/src/string.c
@@ -3,38 +3,47 @@
 #ifndef __ISA_NATIVE__
 
 size_t strlen(const char *s) {
+		TODO();
   return 0;
 }
 
 char *strcpy(char* dst,const char* src) {
+		TODO();
   return NULL;
 }
 
 char* strncpy(char* dst, const char* src, size_t n) {
+		TODO();
   return NULL;
 }
 
 char* strcat(char* dst, const char* src) {
+		TODO();
   return NULL;
 }
 
 int strcmp(const char* s1, const char* s2) {
+		TODO();
   return 0;
 }
 
 int strncmp(const char* s1, const char* s2, size_t n) {
+		TODO();
   return 0;
 }
 
 void* memset(void* v,int c,size_t n) {
+		TODO();
   return NULL;
 }
 
 void* memcpy(void* out, const void* in, size_t n) {
+    TODO();
   return NULL;
 }
 
 int memcmp(const void* s1, const void* s2, size_t n){
+		TODO();
   return 0;
 }
 
