8f1082a55f980384aa0f963e3c2ee50c08e81149
tracer-ics2018 <tracer@njuics.org>
2018-10-08 11:56:07 +0800

    >  compile 171860658 fermat Linux bad 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  11:56:07 up  7:35,  4 users,  load average: 0.14, 0.09, 0.05 63679fd52438f66034c8463c626e1279b4a7f00f

--- nemu/src/cpu/decode/decode.c
+++ nemu/src/cpu/decode/decode.c
@@ -297,6 +297,8 @@ make_DHelper(in_dx2a) {
   id_src->type = OP_TYPE_REG;
   id_src->reg = R_DX;
   rtl_lr(&id_src->val, R_DX, 2);
+	printf("%u\n",id_src->val);
+	assert(0);
 #ifdef DEBUG
   sprintf(id_src->str, "(%%dx)");
 #endif
