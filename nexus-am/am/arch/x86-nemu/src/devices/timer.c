f8f7954aa59e37ae7ed74357c45fcc1e97983465
tracer-ics2018 <tracer@njuics.org>
2018-10-08 21:09:35 +0800

    >  run 171860658 fermat Linux bad 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  21:09:35 up  1:00,  4 users,  load average: 0.06, 0.09, 0.09 f601ab93ba75adb14dc3dcf75d09fa6a109dac0d

--- nexus-am/am/arch/x86-nemu/src/devices/timer.c
+++ nexus-am/am/arch/x86-nemu/src/devices/timer.c
@@ -6,8 +6,10 @@ size_t timer_read(uintptr_t reg, void *buf, size_t size) {
   switch (reg) {
     case _DEVREG_TIMER_UPTIME: {
       _UptimeReg *uptime = (_UptimeReg *)buf;
-      uptime->hi = 0;
-      uptime->lo = 0;
+			if (uptime->lo == 0xffffffff){
+					uptime->hi ++;
+			}
+			uptime->lo ++;
       return sizeof(_UptimeReg);
     }
     case _DEVREG_TIMER_DATE: {
