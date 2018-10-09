86022cf58d610e1a3c653ef7d94cb975ba7e317e
tracer-ics2018 <tracer@njuics.org>
2018-10-10 01:21:53 +0800

    >  run 171860658 fermat Linux bad 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  01:21:53 up 22:40,  4 users,  load average: 0.18, 0.24, 0.30 de1c015f1a0a0ec078ec135ae3d94a278d4f5bd2

--- nexus-am/apps/litenes/src/ppu.c
+++ nexus-am/apps/litenes/src/ppu.c
@@ -283,8 +283,7 @@ void ppu_draw_sprite_scanline()
                 if (do_update) {
 					log("call ppu_ram_read\n");
                     int idx = ppu_ram_read(palette_address + color);
-										printf("%d ",idx);
-                    if (PPU_SPRRAM[n + 2] & 0x20) {
+                    if (PPU_SPRRAM[n + 2] & 0x20) {	
                         draw(screen_x, sprite_y + y_in_tile + 1, idx); // bbg
                     }
                     else {
