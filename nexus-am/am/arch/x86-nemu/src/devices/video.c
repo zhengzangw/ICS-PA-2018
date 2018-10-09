98ab6eae5b18af4a9004d09a50032cde57406ce0
tracer-ics2018 <tracer@njuics.org>
2018-10-09 22:29:58 +0800

    >  run 171860658 fermat Linux bad 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  22:29:58 up 19:59,  4 users,  load average: 0.44, 0.55, 0.32 21224b1f87dab4be1a1c4f1585edc768cefc32cc

--- nexus-am/am/arch/x86-nemu/src/devices/video.c
+++ nexus-am/am/arch/x86-nemu/src/devices/video.c
@@ -23,7 +23,8 @@ size_t video_write(uintptr_t reg, void *buf, size_t size) {
     case _DEVREG_VIDEO_FBCTL: {
       _FBCtlReg *ctl = (_FBCtlReg *)buf;
 			uint32_t w = screen_width();
-			printf("h=%d,w=%d\n",ctl->h,ctl->w);
+			uint32_t h = screen_height();
+			printf("h=%d,w=%d\n;sh=%d,sw=%d",ctl->h,ctl->w,h,w);
 			for (int i=0;i<ctl->h;++i)
 					for (int j=0;j<ctl->w;++j)
 							fb[(i+ctl->x)*w+j+ctl->y] = ctl->pixels[i*ctl->w+j];
