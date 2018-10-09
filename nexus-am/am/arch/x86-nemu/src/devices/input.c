fb4384667627bc95b3f26b4a803ff9cf0bbb1504
tracer-ics2018 <tracer@njuics.org>
2018-10-09 16:21:00 +0800

    >  run 171860658 fermat Linux bad 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  16:21:00 up 14:31,  4 users,  load average: 0.09, 0.06, 0.07 9643aa4c43a5529d62f19443e26ffdb8835b3bc

--- nexus-am/am/arch/x86-nemu/src/devices/input.c
+++ nexus-am/am/arch/x86-nemu/src/devices/input.c
@@ -6,6 +6,17 @@ size_t input_read(uintptr_t reg, void *buf, size_t size) {
   switch (reg) {
     case _DEVREG_INPUT_KBD: {
       _KbdReg *kbd = (_KbdReg *)buf;
+			uint32_t code = inl(0x60);
+			if (code & 0x8000) {
+					// make code
+					kbd->keycode = code ^ 0x8000;
+					kbd->keydown = 1;
+			} else {
+					// break code
+					kbd->keycode = _KEY_NONE;
+					kbd->keydown = 0;
+			}
+
       kbd->keydown = 0;
       kbd->keycode = _KEY_NONE;
       return sizeof(_KbdReg);
