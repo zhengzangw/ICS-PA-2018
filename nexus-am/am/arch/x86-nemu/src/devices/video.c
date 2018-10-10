9387f4176e04a1ab5937226a51423091ee34d4a8
tracer-ics2018 <tracer@njuics.org>
2018-10-10 11:04:56 +0800

    >  compile 171860658 fermat Linux bad 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  11:04:56 up 1 day,  2:57,  7 users,  load average: 0.00, 0.00, 0.00 cd4345e1732d6b6cf9bdcdcd6b473152694c3b4

--- nexus-am/am/arch/x86-nemu/src/devices/video.c
+++ nexus-am/am/arch/x86-nemu/src/devices/video.c
@@ -26,7 +26,7 @@ size_t video_write(uintptr_t reg, void *buf, size_t size) {
 		  //printf("x=%d,y=%d,h=%d,w=%d\n",ctl->x,ctl->y,ctl->h,ctl->w);
 			for (int i=0;i<ctl->h;++i)
 					for (int j=0;j<ctl->w;++j){
-							fb[(i+ctl->x)*w+j+ctl->y] = ctl->pixels[i*ctl->w+j];
+							fb[(i+ctl->y)*w+j+ctl->x] = ctl->pixels[i*ctl->w+j];
 					}
 
       if (ctl->sync) {
