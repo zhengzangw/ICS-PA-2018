cbe61de16d42b8cdda8bb5085f9bfc030a6dbeae
tracer-ics2018 <tracer@njuics.org>
2018-10-10 18:15:15 +0800

    >  compile 171860658 fermat Linux bad 4.9.0-7-amd64 #1 SMP Debian 4.9.110-3+deb9u2 (2018-08-13) x86_64 GNU/Linux  18:15:15 up 1 day,  7:19,  7 users,  load average: 0.19, 0.11, 0.03 d01f4cb579f265ed4c12d40f760422bd840aaee7

--- nemu/include/cpu/rtl.h
+++ nemu/include/cpu/rtl.h
@@ -13,7 +13,7 @@ bool interpret_relop(uint32_t relop, const rtlreg_t src1, const rtlreg_t src2);
 
 /* RTL basic instructions */
 
-static inline void interpret_rtl_li(rtlreg_t* dest, uint32_t imm) {
+void interpret_rtl_li(rtlreg_t* dest, uint32_t imm) {
   *dest = imm;
 }
 
@@ -153,7 +153,7 @@ static inline void rtl_not(rtlreg_t *dest, const rtlreg_t* src1) {
   rtl_xori(dest, src1, 0xffffffff);
 }
 
-inline void rtl_sext(rtlreg_t* dest, const rtlreg_t* src1, int width) {
+static inline void rtl_sext(rtlreg_t* dest, const rtlreg_t* src1, int width) {
   // dest <- signext(src1[(width * 8 - 1) .. 0])
   switch (width){
 			case 4: *dest = *(int32_t *)src1; return;
