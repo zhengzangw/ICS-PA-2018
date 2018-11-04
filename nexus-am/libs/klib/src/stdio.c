830eb1235366596e0d8d369435dce543fd751f2e
tracer-ics2018 <tracer@njuics.org>
2018-11-04 20:34:39 +0800

    >  run 171860658 fermat Linux ics 4.9.0-8-amd64 #1 SMP Debian 4.9.110-3+deb9u6 (2018-10-08) x86_64 GNU/Linux  20:34:39 up 21:27,  5 users,  load average: 0.03, 0.09, 0.03 cc40fd3d34ff6b9ef6c1f2b2e1f84b4d4225ba0

--- nexus-am/libs/klib/src/stdio.c
+++ nexus-am/libs/klib/src/stdio.c
@@ -214,7 +214,7 @@ repeat:
 							continue;
 
 					case 'p':
-							base = 16;
+							//base = 16;
 							if (field_width == -1) {
 									field_width = 2 * sizeof(void *);
 									flags |= ZEROPAD;
