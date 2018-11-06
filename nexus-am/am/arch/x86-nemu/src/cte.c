1ca7c6014e0a8eabf7fb63da682bcc1dbf372010
tracer-ics2018 <tracer@njuics.org>
2018-11-06 17:00:24 +0800

    >  run 171860658 fermat Linux ics 4.9.0-8-amd64 #1 SMP Debian 4.9.110-3+deb9u6 (2018-10-08) x86_64 GNU/Linux  17:00:24 up 2 days,  7:20,  5 users,  load average: 0.08, 0.08, 0.06 348dfeeeb84312d87b1974f7cd9a104d1d8eeb80

--- nexus-am/am/arch/x86-nemu/src/cte.c
+++ nexus-am/am/arch/x86-nemu/src/cte.c
@@ -13,7 +13,7 @@ _Context* irq_handle(_Context *tf) {
 
   if (user_handler) {
     _Event ev;
-		printf("%u\n", tf->irq);
+		printf("%#x\n", tf->irq);
     switch (tf->irq) {
 		  case 0x80: ev.event = _EVENT_SYSCALL; break;
       case 0x81: ev.event = _EVENT_YIELD; break; 
