2bc3d064550f30f29aa3d705e55271d4e4430b7f
171860658-Zheng Zangwei <zzw@smail.nju.edu.cn>
2018-11-06 10:49:01 +0800

    feat(cpu) implement lidt

--- nemu/src/cpu/intr.c
+++ nemu/src/cpu/intr.c
@@ -5,12 +5,9 @@ void raise_intr(uint8_t NO, vaddr_t ret_addr) {
   /* TODO: Trigger an interrupt/exception with ``NO''.
    * That is, use ``NO'' to index the IDT.
    */
- Log("%u", NO);
  rtlreg_t addr = cpu.idtr.addr;
- rtlreg_t tt = addr + NO*8;
- rtl_lm(&t3, &tt, 4);
- Log("addr = %u", tt);
- Log("%x", t3);
+ t2 = addr + NO*8;
+ rtl_lm(&t3, &t2, 4);
 
 		
  TODO();
