0cfa3c30c1cb1a91fc0a94253e8916bb0eb52a4a
tracer-ics2018 <tracer@njuics.org>
2018-11-06 19:43:29 +0800

    >  run 171860658 fermat Linux ics 4.9.0-8-amd64 #1 SMP Debian 4.9.110-3+deb9u6 (2018-10-08) x86_64 GNU/Linux  19:43:29 up 2 days,  9:35,  5 users,  load average: 0.05, 0.06, 0.01 6774e0bc824538eeef5d1e857b3dc801e247889c

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -6,6 +6,7 @@ _Context* do_syscall(_Context *c) {
   a[0] = c->GPR1;
 
   switch (a[0]) {
+		case SYS_exit : _halt(c->GPR2); break;
 		case SYS_yield: _yield(); c->GPR1 = 0; break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
