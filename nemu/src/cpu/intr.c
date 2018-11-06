f21101b355e0ce02129e440c2fc1aa266528fd5a
tracer-ics2018 <tracer@njuics.org>
2018-11-06 10:38:34 +0800

    >  compile 171860658 fermat Linux ics 4.9.0-8-amd64 #1 SMP Debian 4.9.110-3+deb9u6 (2018-10-08) x86_64 GNU/Linux  10:38:34 up 2 days,  1:56,  5 users,  load average: 0.20, 0.13, 0.10 75eb349adcf127f364918f8a3667ab39a40d4844

--- nemu/src/cpu/intr.c
+++ nemu/src/cpu/intr.c
@@ -5,11 +5,9 @@ void raise_intr(uint8_t NO, vaddr_t ret_addr) {
   /* TODO: Trigger an interrupt/exception with ``NO''.
    * That is, use ``NO'' to index the IDT.
    */
- Log("%u", NO);
  rtlreg_t addr = cpu.idtr.addr;
- rtlreg_t tt = addr + NO*8;
- rtl_lm(&t3, &tt, 4);
- Log("addr = %u", tt);
+ t3 = addr + NO*8;
+ rtl_lm(&t3, &t3, 4);
  Log("%u", t3);
 
 		
