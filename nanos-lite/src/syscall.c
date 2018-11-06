6e0e75de311f881df3c4b8f3a05b2baf140e43e8
tracer-ics2018 <tracer@njuics.org>
2018-11-06 22:53:50 +0800

    >  run 171860658 fermat Linux ics 4.9.0-8-amd64 #1 SMP Debian 4.9.110-3+deb9u6 (2018-10-08) x86_64 GNU/Linux  22:53:50 up 2 days, 12:46,  5 users,  load average: 0.04, 0.03, 0.06 1dafec3a81d965a15e0510ca53d1f047a1e25196

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -1,6 +1,8 @@
 #include "common.h"
 #include "syscall.h"
 
+extern int end;
+
 _Context* do_syscall(_Context *c) {
   uintptr_t a[4];
   a[0] = c->GPR1;
@@ -10,6 +12,7 @@ _Context* do_syscall(_Context *c) {
 		case SYS_yield: _yield(); c->eax = 0; break;
 		case SYS_write: 
 				Log("write: %s", (char *)c->GPR3);
+				Log("%u", end);
 				if (c->GPR2==1||c->GPR2==2){
 						for (size_t i=0;i<c->GPR4;++i){
 								_putc(*(char *)(c->GPR3+i));
@@ -18,6 +21,10 @@ _Context* do_syscall(_Context *c) {
 						assert(0);
 				}
 				c->eax = c->GPR4;
+				break;
+		case SYS_brk:
+
+				c->eax = 0;
 				break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
