9ff244250d969f688b48cafe5dd2e20a467aa4b9
tracer-ics2018 <tracer@njuics.org>
2018-11-06 14:11:49 +0800

    >  run 171860658 fermat Linux ics 4.9.0-8-amd64 #1 SMP Debian 4.9.110-3+deb9u6 (2018-10-08) x86_64 GNU/Linux  14:11:49 up 2 days,  4:31,  5 users,  load average: 0.29, 0.14, 0.04 4c099425d6bd928515f25f22dc487e2034aca40d

--- nexus-am/am/arch/x86-nemu/src/cte.c
+++ nexus-am/am/arch/x86-nemu/src/cte.c
@@ -14,14 +14,14 @@ _Context* irq_handle(_Context *tf) {
 	mylog(ebp);
 	mylog(esp);
 	mylog(ebx);
-	mylog(edx);
+	/*mylog(edx);
 	mylog(ecx);
 	mylog(eax);
 	mylog(irq);
 	mylog(err);
 	mylog(eip);
 	mylog(cs);
-	mylog(eflags);
+	mylog(eflags);*/
   if (user_handler) {
     _Event ev;
     switch (tf->irq) {
