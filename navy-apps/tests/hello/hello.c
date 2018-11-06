f555bb735914a43cf6a60afc6fcee86535580920
tracer-ics2018 <tracer@njuics.org>
2018-11-06 21:43:18 +0800

    >  run 171860658 fermat Linux ics 4.9.0-8-amd64 #1 SMP Debian 4.9.110-3+deb9u6 (2018-10-08) x86_64 GNU/Linux  21:43:18 up 2 days, 11:35,  5 users,  load average: 0.13, 0.12, 0.09 78a6612b5a40ab7b192a5b9094717108f738940c

--- navy-apps/tests/hello/hello.c
+++ navy-apps/tests/hello/hello.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 
 int main() {
+  printf("%d",10);
   write(1, "Hello World!\n", 13);
   int i = 2;
   volatile int j = 0;
@@ -9,7 +10,7 @@ int main() {
     j ++;
     if (j == 10000) {
 				i++;
-      printf("Hello World from Navy-apps for the %uth time!\n", 9);
+      printf("Hello World from Navy-apps for the %uth time!\n", 999);
       j = 0;
     }
   }
