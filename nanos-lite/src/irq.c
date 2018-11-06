8ccfb8d664006813f8391fbbd816f5de4861ebed
tracer-ics2018 <tracer@njuics.org>
2018-11-06 19:58:58 +0800

    >  run 171860658 fermat Linux ics 4.9.0-8-amd64 #1 SMP Debian 4.9.110-3+deb9u6 (2018-10-08) x86_64 GNU/Linux  19:58:58 up 2 days,  9:51,  5 users,  load average: 0.23, 0.09, 0.02 41e2932fcc5107e2f5b8f6dc756f8b5bd60b419

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -3,8 +3,8 @@
 
 static _Context* do_event(_Event e, _Context* c) {
   switch (e.event) {
-		case _EVENT_SYSCALL: Log("SYSCALL event\n"); do_syscall(c); break;
-		case _EVENT_YIELD: Log("YIELD event\n"); break;
+		case _EVENT_SYSCALL: Log("SYSCALL event"); do_syscall(c); break;
+		case _EVENT_YIELD: Log("YIELD event"); break;
     default: panic("Unhandled event ID = %d", e.event);
   }
 
