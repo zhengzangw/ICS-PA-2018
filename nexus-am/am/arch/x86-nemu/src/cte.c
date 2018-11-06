dc9002669846e329492b4bbf495a19e02beff306
tracer-ics2018 <tracer@njuics.org>
2018-11-06 10:40:00 +0800

    >  run 171860658 fermat Linux ics 4.9.0-8-amd64 #1 SMP Debian 4.9.110-3+deb9u6 (2018-10-08) x86_64 GNU/Linux  10:40:00 up 2 days,  1:58,  5 users,  load average: 0.39, 0.19, 0.12 23166265304ddeb130b3c9917805ea73617681de

--- nexus-am/am/arch/x86-nemu/src/cte.c
+++ nexus-am/am/arch/x86-nemu/src/cte.c
@@ -33,6 +33,9 @@ int _cte_init(_Context*(*handler)(_Event, _Context*)) {
 
   // -------------------- system call --------------------------
   idt[0x81] = GATE(STS_TG32, KSEL(SEG_KCODE), vectrap, DPL_KERN);
+	printf("%u\n", idt+0x81);
+	printf("%u\n", idt);
+	printf("%u\n", idt[0x81]);
 
   set_idt(idt, sizeof(idt));
 
