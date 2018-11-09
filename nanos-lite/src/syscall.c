20336b95b2510baac73bcccf7b247770e2264677
tracer-ics2018 <tracer@njuics.org>
2018-11-10 00:11:50 +0800

    >  run 171860658 fermat Linux ics 4.9.0-8-amd64 #1 SMP Debian 4.9.110-3+deb9u6 (2018-10-08) x86_64 GNU/Linux  00:11:50 up 18:51,  5 users,  load average: 0.14, 0.16, 0.09 3e6fc785b68626019fb0dacad92562054b5d200e

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -9,24 +9,24 @@ _Context* do_syscall(_Context *c) {
 
   switch (a[0]) {
 		case SYS_exit : _halt(c->GPR2); break;
-		case SYS_yield: _yield(); c->eax = 0; break;
+		case SYS_yield: _yield(); c->GPR1 = 0; break;
 		case SYS_brk:
-				c->eax = 0;
+				c->GPR1 = 0;
 				break;
 		case SYS_open:
-		    c->eax = fs_open((char *)c->GPR2, (int)c->GPR3, (int)c->GPR4);
+		    c->GPR1 = fs_open((char *)c->GPR2, (int)c->GPR3, (int)c->GPR4);
 				break;
 		case SYS_close:
-				c->eax = fs_close((int)c->GPR2);
+				c->GPR1 = fs_close((int)c->GPR2);
 				break;
 		case SYS_lseek:
-				c->eax = fs_lseek((int)c->GPR2, (off_t)c->GPR3, (int)c->GPR4);
+				c->GPR1 = fs_lseek((int)c->GPR2, (off_t)c->GPR3, (int)c->GPR4);
 				break;
 		case SYS_read:
-				c->eax = fs_read((int)c->GPR2, (void *)c->GPR3, (size_t)c->GPR4);
+				c->GPR1 = fs_read((int)c->GPR2, (void *)c->GPR3, (size_t)c->GPR4);
 				break;
 		case SYS_write: 
-				c->eax = fs_write((int)c->GPR2, (void *)c->GPR3, (size_t)c->GPR4);
+				c->GPR1 = fs_write((int)c->GPR2, (void *)c->GPR3, (size_t)c->GPR4);
 				break;
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
