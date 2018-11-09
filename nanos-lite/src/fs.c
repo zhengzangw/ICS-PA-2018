cdb06ca130dad52471b1fecf0fd39ea32a563638
tracer-ics2018 <tracer@njuics.org>
2018-11-10 00:09:30 +0800

    >  run 171860658 fermat Linux ics 4.9.0-8-amd64 #1 SMP Debian 4.9.110-3+deb9u6 (2018-10-08) x86_64 GNU/Linux  00:09:30 up 18:49,  5 users,  load average: 0.12, 0.13, 0.07 fb6721a78474ad6fab9ee7e6b18abb6fb0fa627

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -57,11 +57,15 @@ int fs_close(int fd){
 }
 
 ssize_t fs_read(int fd, void *buf, size_t len){
-		size_t left = file_table[fd].size - file_table[fd].open_offset;
-		size_t real_len = len < left ? len : left;
-		ramdisk_read(buf, file_table[fd].disk_offset + file_table[fd].open_offset, real_len);
-		file_table[fd].open_offset += real_len;
-		return real_len;
+		if (file_table[fd].read!=NULL){
+				return file_table[fd].read(buf, 0, len);
+		} else {
+		  size_t left = file_table[fd].size - file_table[fd].open_offset;
+		  size_t real_len = len < left ? len : left;
+		  ramdisk_read(buf, file_table[fd].disk_offset + file_table[fd].open_offset, real_len);
+		  file_table[fd].open_offset += real_len;
+		  return real_len;
+		}
 }
 
 ssize_t fs_write(int fd, const void *buf, size_t len){	
