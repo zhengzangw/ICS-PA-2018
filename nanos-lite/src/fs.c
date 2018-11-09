4d82d091852143d5622189f77b8e439407e65274
tracer-ics2018 <tracer@njuics.org>
2018-11-09 20:47:12 +0800

    >  run 171860658 fermat Linux ics 4.9.0-8-amd64 #1 SMP Debian 4.9.110-3+deb9u6 (2018-10-08) x86_64 GNU/Linux  20:47:12 up 15:27,  5 users,  load average: 0.14, 0.11, 0.13 5799af5547c6acb594f9e1886f3f5e1e29837413

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -40,6 +40,7 @@ void init_fs() {
 }
 
 int fs_open(const char *pathname, int flags, int mode){
+		Log("open");
 		int fd = -1;
 		for (int i=0; i<NR_FILES; ++i){
 				if (strcmp(file_table[i].name, pathname)==0){
