55ff451300f41966ab9d6c2446eadfaf9bdb58d2
tracer-ics2018 <tracer@njuics.org>
2018-11-09 20:43:29 +0800

    >  run 171860658 fermat Linux ics 4.9.0-8-amd64 #1 SMP Debian 4.9.110-3+deb9u6 (2018-10-08) x86_64 GNU/Linux  20:43:29 up 15:23,  5 users,  load average: 0.09, 0.09, 0.14 f190c2518a2b9d3ef43e3efb71d284c1e51ba0f

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -27,7 +27,6 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
 
 void naive_uload(PCB *pcb, const char *filename) {
   uintptr_t entry = loader(pcb, filename);
-	Log("IN");
   ((void(*)())entry) ();
 }
 
