b01727e3c773c3a9a6ad648377a4b6b7cdfa8afd
tracer-ics2018 <tracer@njuics.org>
2018-11-09 22:29:30 +0800

    >  run 171860658 fermat Linux ics 4.9.0-8-amd64 #1 SMP Debian 4.9.110-3+deb9u6 (2018-10-08) x86_64 GNU/Linux  22:29:30 up 17:09,  5 users,  load average: 0.36, 0.09, 0.03 872c57544143db91ad816ec6eb638d36a0d47610

--- navy-apps/tests/text/text.c
+++ navy-apps/tests/text/text.c
@@ -11,6 +11,8 @@ int main() {
   assert(size == 5000);
 
   fseek(fp, 500 * 5, SEEK_SET);
+	long size = ftell(fp);
+	printf("%d\n", size);
   int i, n;
   for (i = 500; i < 1000; i ++) {
     fscanf(fp, "%d", &n);
