cb2d36769dacfe0057832ea14c7210bd9673857c
tracer-ics2018 <tracer@njuics.org>
2018-11-09 20:11:50 +0800

    >  run 171860658 fermat Linux ics 4.9.0-8-amd64 #1 SMP Debian 4.9.110-3+deb9u6 (2018-10-08) x86_64 GNU/Linux  20:11:50 up 14:51,  5 users,  load average: 0.11, 0.29, 0.60 3860956712178d85eb423660888d8e4b6ea2eed6

--- navy-apps/libs/libos/src/nanos.c
+++ navy-apps/libs/libos/src/nanos.c
@@ -28,7 +28,7 @@ void _exit(int status) {
 }
 
 int _open(const char *path, int flags, mode_t mode) {
-  return _syscall_(SYS_open, fd, flags, mode);
+  return _syscall_(SYS_open, path, flags, mode);
 }
 
 int _write(int fd, void *buf, size_t count){
