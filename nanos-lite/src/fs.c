4e8a94648a9872992d42db6986c210a637a4c9c8
tracer-ics2018 <tracer@njuics.org>
2018-11-09 21:59:51 +0800

    >  run 171860658 fermat Linux ics 4.9.0-8-amd64 #1 SMP Debian 4.9.110-3+deb9u6 (2018-10-08) x86_64 GNU/Linux  21:59:51 up 16:39,  5 users,  load average: 0.10, 0.08, 0.03 dcb94186fe4dbb9e46bdcb23890294cd2ed6f8c3

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -57,9 +57,10 @@ int fs_close(int fd){
 }
 
 ssize_t fs_read(int fd, void *buf, size_t len){
-		Log("read");
-		int real_len = len < file_table[fd].size ? len : file_table[fd].size;
+		size_t left = file_table[fd].disk_offset + file_table[fd].size - file_table[fd].open_offset;
+		size_t real_len = len < left ? len : left;
 		ramdisk_read(buf, file_table[fd].disk_offset+file_table[fd].open_offset, real_len);
+		file_table[fd].open_offset += real_len;
 		return real_len;
 }
 
