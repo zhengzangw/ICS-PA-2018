8b9c3a1ae7bdab3985567860e731c152a1692d29
tracer-ics2018 <tracer@njuics.org>
2018-11-10 18:56:54 +0800

    >  run 171860658 fermat Linux ics 4.9.0-8-amd64 #1 SMP Debian 4.9.110-3+deb9u6 (2018-10-08) x86_64 GNU/Linux  18:56:54 up  5:16,  9 users,  load average: 0.16, 0.11, 0.09 ec29e9053da9d275c8a42fbc781a1ba15eee5f33

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -30,7 +30,7 @@ size_t dispinfo_read(void *buf, size_t offset, size_t len) {
 
 size_t fb_write(const void *buf, size_t offset, size_t len) {
   Log("offset = %u", offset);
-  int x = offset/4 / screen_width();
+  int x = offset/4 / screen_height();
 	Log("x = %u", x);
 	int y = offset/4 - x;
 	Log("y = %u", y);
@@ -44,7 +44,5 @@ void init_device() {
 
   // TODO: print the string to array `dispinfo` with the format
   // described in the Navy-apps convention
-	int width = screen_width();
-	int height = screen_height();
-	sprintf(dispinfo, "WIDTH:%d\nHEIGHT:%d", width, height);
+	sprintf(dispinfo, "WIDTH:%d\nHEIGHT:%d", screen_width(), screen_height());
 }
