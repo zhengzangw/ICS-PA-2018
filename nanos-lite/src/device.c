cc07ab03e7086050bd3e7916ceb49b2e7f6a4c1a
tracer-ics2018 <tracer@njuics.org>
2018-11-10 17:30:56 +0800

    >  run 171860658 fermat Linux ics 4.9.0-8-amd64 #1 SMP Debian 4.9.110-3+deb9u6 (2018-10-08) x86_64 GNU/Linux  17:30:56 up  3:50,  8 users,  load average: 0.02, 0.06, 0.02 b5d5e94ab97c51e3a86f19e1b48dce66118c84

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -23,12 +23,16 @@ size_t events_read(void *buf, size_t offset, size_t len) {
 static char dispinfo[128] __attribute__((used));
 
 size_t dispinfo_read(void *buf, size_t offset, size_t len) {
+  strncpy(buf, dispinfo+offset, len);
   return 0;
 }
 
 size_t fb_write(const void *buf, size_t offset, size_t len) {
-  int x = offset / 9600 ;
-	int y = offset - x;
+  Log("offset = %u", offset);
+  int x = offset/32 / screen_width();
+	Log("x = %u", x);
+	int y = offset/32 - x;
+	Log("y = %u", y);
 	draw_rect((uint32_t*)buf, x, y, 1, len);
   return 0;
 }
@@ -39,4 +43,7 @@ void init_device() {
 
   // TODO: print the string to array `dispinfo` with the format
   // described in the Navy-apps convention
+	int width = screen_width();
+	int height = screen_height();
+	sprintf(dispinfo, "WIDTH : %d\nHEIGHT:%d\n", width, height);
 }
--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -39,7 +39,7 @@ static Finfo file_table[] __attribute__((used)) = {
 
 void init_fs() {
   // TODO: initialize the size of /dev/fb
-  file_table[3].size = 3840000;	
+  file_table[3].size = screen_width()*screen_height()*4;	
 }
 
 int fs_open(const char *pathname, int flags, int mode){
