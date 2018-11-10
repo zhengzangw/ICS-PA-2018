51472b735a76304991cf8bb3e2e687fb0c2b7251
tracer-ics2018 <tracer@njuics.org>
2018-11-10 19:36:18 +0800

    >  run 171860658 fermat Linux ics 4.9.0-8-amd64 #1 SMP Debian 4.9.110-3+deb9u6 (2018-10-08) x86_64 GNU/Linux  19:36:18 up  5:55,  9 users,  load average: 0.07, 0.07, 0.02 245828cbf5b48c2cf68033bca738059551cc249c

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -31,9 +31,9 @@ size_t dispinfo_read(void *buf, size_t offset, size_t len) {
 size_t fb_write(const void *buf, size_t offset, size_t len) {
   Log("offset = %u", offset);
 	int offset_m = offset / 32;
-  int x = offset_m / screen_width();
+  int x = offset_m % screen_width();
 	Log("x = %u", x);
-	int y = offset_m % screen_width();
+	int y = offset_m / screen_width();
 	Log("y = %u", y);
 	draw_rect((uint32_t*)buf, x, y, 1, len);
   return len;
