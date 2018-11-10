a7d4bacb82b51f657ca34b6d629d354b636d2f10
tracer-ics2018 <tracer@njuics.org>
2018-11-10 17:47:26 +0800

    >  run 171860658 fermat Linux ics 4.9.0-8-amd64 #1 SMP Debian 4.9.110-3+deb9u6 (2018-10-08) x86_64 GNU/Linux  17:47:26 up  4:06,  9 users,  load average: 0.11, 0.11, 0.06 5279f44743156ea946a54893ba6d1b951efe797f

--- navy-apps/libs/libndl/src/ndl.c
+++ navy-apps/libs/libndl/src/ndl.c
@@ -133,7 +133,7 @@ static void get_display_info() {
   while (fgets(buf, 128, dispinfo)) {
     *(delim = strchr(buf, ':')) = '\0';
     sscanf(buf, "%s", key);
-		printf(key);
+		printf(buf);
     sscanf(delim + 1, "%s", value);
     if (strcmp(key, "WIDTH") == 0) sscanf(value, "%d", &screen_w);
     if (strcmp(key, "HEIGHT") == 0) sscanf(value, "%d", &screen_h);
