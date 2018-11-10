218b487ae9a104063b83b23240bbdbe76c714241
tracer-ics2018 <tracer@njuics.org>
2018-11-10 18:42:30 +0800

    >  run 171860658 fermat Linux ics 4.9.0-8-amd64 #1 SMP Debian 4.9.110-3+deb9u6 (2018-10-08) x86_64 GNU/Linux  18:42:30 up  5:01,  9 users,  load average: 0.08, 0.15, 0.08 b5651ebf1d77cd5acaf21f9b84e1f7e8d14cb9

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -66,7 +66,7 @@ int fs_close(int fd){
 
 ssize_t fs_read(int fd, void *buf, size_t len){
 		if (file_table[fd].read!=NULL){
-		  size_t real_len = file_table[fd].read(buf, 0, len);
+		  size_t real_len = file_table[fd].read(buf, file_table[fd].open_offset, len);
 			file_table[fd].open_offset += real_len;
 			return real_len;
 		} else {
