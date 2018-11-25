01904f58c1d8e517eb0ac80c281e4cddd0cf726e
tracer-ics2018 <tracer@njuics.org>
2018-11-25 21:26:55 +0800

    >  run 171860658 fermat Linux ics 4.9.0-8-amd64 #1 SMP Debian 4.9.130-2 (2018-10-27) x86_64 GNU/Linux  21:26:55 up 6 days,  7:36,  2 users,  load average: 0.38, 0.38, 0.34 a43ae8e0fdcd8e6293bfe75c7feb7516704fc1ae

--- nanos-lite/src/device.c
+++ nanos-lite/src/device.c
@@ -19,11 +19,11 @@ static const char *keyname[256] __attribute__((used)) = {
 size_t events_read(void *buf, size_t offset, size_t len) {
   int key = read_key();
 	if (key) {
-			if (key&0x8000){ 
-				 	sprintf(buf, "kd %s\n", keyname[key&0x7fff]);
+			if (key&0x8000){
+				sprintf(buf, "kd RETURN\n");
 			}
 			else{
-					sprintf(buf, "kd RETURN\n");
+				sprintf(buf, "ku %s\n", keyname[key&0x7fff]);
 			}
 	} else {
 			sprintf(buf, "t %u\n", uptime());
