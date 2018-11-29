60af8aa01bf0aa82bd628fb7cdde229aeb9d4025
tracer-ics2018 <tracer@njuics.org>
2018-11-30 01:26:34 +0800

    >  compile 171860658 fermat Linux ics 4.9.0-8-amd64 #1 SMP Debian 4.9.130-2 (2018-10-27) x86_64 GNU/Linux  01:26:34 up  9:33,  4 users,  load average: 0.50, 0.20, 0.12 26d7742d420277386635e066047f5988b31f239

--- nemu/src/monitor/diff-test/diff-test.c
+++ nemu/src/monitor/diff-test/diff-test.c
@@ -78,9 +78,9 @@ void difftest_attach(){
     idtr.size = cpu.idtr.size;
     idtr.addr = cpu.idtr.addr;
     ref_difftest_memcpy_from_dut(0x7c00, &idtr, sizeof(idtr));
-    CPU_state tmp = cpu;
-    int8_t inst[] = {0x0f,0x01,0xD1,0x00,0x7e,0x00,0x00};
+    int8_t inst[] = {0x0f,0x01,0x1D,0x00,0x7e,0x00,0x00};
     ref_difftest_memcpy_from_dut(0x7e40, inst, sizeof(inst));
+    CPU_state tmp = cpu;
     tmp.eip = 0x7e40;
     ref_difftest_setregs(&tmp);
     ref_difftest_exec(1);
