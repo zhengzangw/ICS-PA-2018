831dd5d1aaf6640fef8cf5ebd60aa38c015f4329
tracer-ics2018 <tracer@njuics.org>
2018-12-12 00:53:44 +0800

    >  compile 171860658 fermat Linux ics 4.9.0-8-amd64 #1 SMP Debian 4.9.130-2 (2018-10-27) x86_64 GNU/Linux  00:53:44 up 2 days, 22:32,  4 users,  load average: 0.05, 0.07, 0.06 7f7e5de27b2194cc96b6bf58da265be145efa91

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -57,12 +57,13 @@ paddr_t page_translation(vaddr_t addr){
 #define CROSS_PGBOUND(addr,len) ((addr>>12)!=((addr+len)>>12))
 
 uint32_t vaddr_read(vaddr_t addr, int len) {
-  if ((addr >> 22)) Log("addr = %x", addr>>22);
+  if ((addr >> 22)>0x20) Log("addr = %x", addr);
   if (PG) {
       if (CROSS_PGBOUND(addr, len)){
         uint32_t lo_len = ((addr+len)&~0xfff) - addr;
         uint32_t hi_len = (addr+len) - ((addr+len)&~0xfff);
         paddr_t lo_paddr = page_translation(addr);
+        if ((((addr+len)&~0xfff) >> 22)>0x20) Log("hi_addr = %x", addr);
         paddr_t hi_paddr = page_translation((addr+len)&~0xfff);
         uint32_t lo = paddr_read(lo_paddr, lo_len);
         uint32_t hi = paddr_read(hi_paddr, hi_len);
@@ -83,6 +84,7 @@ void vaddr_write(vaddr_t addr, uint32_t data, int len) {
         uint32_t lo_len = ((addr+len)&~0xfff) - addr;
         uint32_t hi_len = (addr+len) - ((addr+len)&~0xfff);
         paddr_t lo_paddr = page_translation(addr);
+        if ((((addr+len)&~0xfff) >> 22)>0x20) Log("hi_addr = %x", addr);
         paddr_t hi_paddr = page_translation((addr+len)&~0xfff);
         paddr_write(lo_paddr, data & ~((-1)<<lo_len), lo_len);
         paddr_write(hi_paddr, data >> lo_len, hi_len);
