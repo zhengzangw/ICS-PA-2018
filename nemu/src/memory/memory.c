1c6030d59f409363b37ccf07cd2f54d39d964314
171860658-Zheng Zangwei <zzw@smail.nju.edu.cn>
2018-12-12 01:23:22 +0800

    PA4.2: get the first part done can not run /bin/init

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -44,7 +44,6 @@ typedef union {
 paddr_t page_translation(vaddr_t addr){
     Vaddr vaddr;
     vaddr.value = addr;
-    if ((vaddr.value >> 22)>=0x20) Log("addr = %x", addr);
     uint32_t pdir   = (cpu.CR3 & ~0xfff) + (vaddr.dir << 2);
     uint32_t pdir_index = paddr_read(pdir, 4);
     Assert(pdir_index&0x1, "addr = %x, pdir = %x, pdir_index = %x, dir = %x",addr, pdir, pdir_index, vaddr.dir);
@@ -58,13 +57,11 @@ paddr_t page_translation(vaddr_t addr){
 #define CROSS_PGBOUND(addr,len) ((addr>>12)!=((addr+len-1)>>12))
 
 uint32_t vaddr_read(vaddr_t addr, int len) {
-  if ((addr >> 22)>=0x20) Log("addr = %x", addr);
   if (PG) {
       if (CROSS_PGBOUND(addr, len)){
         uint32_t lo_len = ((addr+len-1)&~0xfff) - addr;
         uint32_t hi_len = (addr+len-1) - ((addr+len-1)&~0xfff);
         paddr_t lo_paddr = page_translation(addr);
-        if ((((addr+len-1)&~0xfff) >> 22)>=0x20) Log("hi_addr = %x", addr);
         paddr_t hi_paddr = page_translation((addr+len-1)&~0xfff);
         uint32_t lo = paddr_read(lo_paddr, lo_len);
         uint32_t hi = paddr_read(hi_paddr, hi_len);
@@ -79,13 +76,11 @@ uint32_t vaddr_read(vaddr_t addr, int len) {
 }
 
 void vaddr_write(vaddr_t addr, uint32_t data, int len) {
-  if ((addr >> 22)>=0x20) Log("addr = %x", addr);
   if (PG) {
       if (CROSS_PGBOUND(addr, len)){
         uint32_t lo_len = ((addr+len-1)&~0xfff) - addr;
         uint32_t hi_len = (addr+len-1) - ((addr+len-1)&~0xfff);
         paddr_t lo_paddr = page_translation(addr);
-        if ((((addr+len-1)&~0xfff) >> 22)>=0x20) Log("hi_addr = %x", addr);
         paddr_t hi_paddr = page_translation((addr+len-1)&~0xfff);
         paddr_write(lo_paddr, data & ~((-1)<<lo_len), lo_len);
         paddr_write(hi_paddr, data >> lo_len, hi_len);
