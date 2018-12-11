bc8a0d3a5d486af41b62e2af100f95747d3f6f07
tracer-ics2018 <tracer@njuics.org>
2018-12-12 02:37:25 +0800

    >  run 171860658 fermat Linux ics 4.9.0-8-amd64 #1 SMP Debian 4.9.130-2 (2018-10-27) x86_64 GNU/Linux  02:37:25 up 3 days, 16 min,  4 users,  load average: 0.08, 0.10, 0.09 4153e1189e09632a6f602ac837c942a8f8e3d3b

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -18,9 +18,9 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
 	int fd = fs_open(filename, 0, 0);
     uint32_t filesz = fs_filesz(fd);
     uint32_t nr_page = filesz / PGSIZE;
-    Log("nr_page = %u",nr_page);
     void *va = (void *)DEFAULT_ENTRY;
     for (int i=0; i < nr_page; ++i){
+        Log("va = %u", va);
         void *pa = new_page(1);
         _map(&pcb->as, va, pa, 1);
         fs_read(fd, pa, filesz>PGSIZE?PGSIZE:filesz);
