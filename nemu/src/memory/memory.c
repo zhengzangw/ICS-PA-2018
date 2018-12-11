4cc95ab83a03421a7d2cdafb191cac34505f6e76
tracer-ics2018 <tracer@njuics.org>
2018-12-12 02:57:22 +0800

    >  compile 171860658 fermat Linux ics 4.9.0-8-amd64 #1 SMP Debian 4.9.130-2 (2018-10-27) x86_64 GNU/Linux  02:57:22 up 3 days, 36 min,  4 users,  load average: 0.65, 0.29, 0.15 4903c057e6b36d0578aaa4d3141d45e75f8dae68

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -45,9 +45,8 @@ paddr_t page_translation(vaddr_t addr){
     Vaddr vaddr;
     vaddr.value = addr;
     uint32_t pdir   = (cpu.CR3 & ~0xfff) + (vaddr.dir << 2);
-    Log("CR3 = %x", cpu.CR3);
     uint32_t pdir_index = paddr_read(pdir, 4);
-    Assert(pdir_index&0x1, "addr = %x, pdir = %x, pdir_index = %x, dir = %x",addr, pdir, pdir_index, vaddr.dir);
+    Assert(pdir_index&0x1, "CR3 = %x, addr = %x, pdir = %x, pdir_index = %x, dir = %x", cpu.CR3, addr, pdir, pdir_index, vaddr.dir);
     uint32_t pte    = (pdir_index & ~0xfff) + (vaddr.page << 2);
     uint32_t pte_index  = paddr_read(pte, 4);
     assert(pte_index&0x1);
