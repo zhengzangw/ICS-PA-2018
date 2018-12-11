8c5d89e6e6bf9de5b482a8188c92bf2b123fc477
tracer-ics2018 <tracer@njuics.org>
2018-12-12 03:41:16 +0800

    >  run 171860658 fermat Linux ics 4.9.0-8-amd64 #1 SMP Debian 4.9.130-2 (2018-10-27) x86_64 GNU/Linux  03:41:16 up 3 days,  1:20,  4 users,  load average: 0.15, 0.17, 0.12 e8dac1513d7cb83b8e8bd77a77989f7077f8a181

--- nanos-lite/src/mm.c
+++ nanos-lite/src/mm.c
@@ -1,4 +1,5 @@
 #include "memory.h"
+#include "proc.h"
 
 static void *pf = NULL;
 
@@ -15,6 +16,15 @@ void free_page(void *p) {
 
 /* The brk() system call handler. */
 int mm_brk(uintptr_t new_brk) {
+  if (current->max_brk < new_brk){
+    uint32_t szneed = new_brk - current->max_brk;
+    uint32_t pgnum = szneed / PGSIZE;
+    for (int i=0; i < pgnum; ++i){
+        //void *pa = new_page(1);
+        panic("new_brk = %x", new_brk);
+        //_map(current->as, va, pa, 1);
+    }
+  }
   return 0;
 }
 
