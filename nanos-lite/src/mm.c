3bb10737c978163529fc29f2d473d4ba68d8d14a
tracer-ics2018 <tracer@njuics.org>
2018-12-12 12:34:05 +0800

    >  run 171860658 fermat Linux ics 4.9.0-8-amd64 #1 SMP Debian 4.9.130-2 (2018-10-27) x86_64 GNU/Linux  12:34:05 up 3 days,  3:37,  4 users,  load average: 0.06, 0.08, 0.02 64fa2d0cd68cd8457cfa66cba25fe0e3dd9017e

--- nanos-lite/src/mm.c
+++ nanos-lite/src/mm.c
@@ -17,9 +17,9 @@ void free_page(void *p) {
 /* The brk() system call handler. */
 int mm_brk(uintptr_t new_brk) {
   Log("in mm_brk");
-  Log("new_brk = %x, max_brk = %x", new_brk, current->max_brk);
+  Log("new_brk = %x, max_brk = %x, cur_brk = %x", new_brk, current->max_brk, current->cur_brk);
   if (current->max_brk < new_brk){
-    uint32_t new_brk_align = (new_brk & ~0xfff) + 0x1000;
+    uint32_t new_brk_align = new_brk & ~0xfff;
     Log("new_brk_align = %x", new_brk_align);
     uint32_t szneed = new_brk_align - current->max_brk;
     uint32_t pgnum = szneed / PGSIZE;
