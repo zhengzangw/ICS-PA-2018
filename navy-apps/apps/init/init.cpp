9d0f20a42fa167c4d3b24880b8f85abc04a7a512
tracer-ics2018 <tracer@njuics.org>
2018-12-12 21:05:48 +0800

    >  run 171860658 fermat Linux ics 4.9.0-8-amd64 #1 SMP Debian 4.9.130-2 (2018-10-27) x86_64 GNU/Linux  21:05:48 up 3 days, 10:43,  4 users,  load average: 0.26, 0.18, 0.16 45a1be1ce4d1fec67f8233cff167886d5d780af3

--- navy-apps/apps/init/init.cpp
+++ navy-apps/apps/init/init.cpp
@@ -65,7 +65,6 @@ static void clear_display();
 static void display_menu(int n);
 
 int main(int argc, char *argv[], char *envp[]) {
-  //printf("A\n");
   if (!freopen("/dev/tty", "r", stdin) ||
       !freopen("/dev/tty", "w", stdout) ||
       !freopen("/dev/tty", "w", stderr)) {
@@ -74,6 +73,7 @@ int main(int argc, char *argv[], char *envp[]) {
   }
 
   open_display();
+  printf("A\n");
 
   evtdev = fopen("/dev/events", "r");
   font = new Font(font_fname);
