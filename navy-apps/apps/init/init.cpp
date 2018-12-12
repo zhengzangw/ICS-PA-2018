6bb19b555a7d0cb820fda4ddc4927194839bf31a
tracer-ics2018 <tracer@njuics.org>
2018-12-13 00:34:11 +0800

    >  run 171860658 fermat Linux ics 4.9.0-8-amd64 #1 SMP Debian 4.9.130-2 (2018-10-27) x86_64 GNU/Linux  00:34:11 up 3 days, 14:11,  4 users,  load average: 0.24, 0.16, 0.18 a159677e69e53ee3e72aca04b34be3f6f182d92c

--- navy-apps/apps/init/init.cpp
+++ navy-apps/apps/init/init.cpp
@@ -113,8 +113,8 @@ int main(int argc, char *argv[], char *envp[]) {
       exec_argv[1] = item->arg1;
       exec_argv[2] = NULL;
       clear_display();
-      printf("\n");
-      printf("\n");
+      printf("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\n");
+      printf("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa\n");
       execve(exec_argv[0], (char**)exec_argv, (char**)envp);
       fprintf(stderr, "\033[31m[ERROR]\033[0m Exec %s failed.\n\n", exec_argv[0]);
     } else {
