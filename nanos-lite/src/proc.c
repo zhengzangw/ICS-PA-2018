98e6aeb914e7778ceca981977585ebdf8d402700
tracer-ics2018 <tracer@njuics.org>
2018-12-12 19:47:02 +0800

    >  run 171860658 fermat Linux ics 4.9.0-8-amd64 #1 SMP Debian 4.9.130-2 (2018-10-27) x86_64 GNU/Linux  19:47:02 up 3 days,  9:24,  4 users,  load average: 0.86, 0.49, 0.25 2653b2cccb3359852a1f5693ed6380a9105e4294

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -22,14 +22,14 @@ void hello_fun(void *arg) {
 void init_proc() {
 	//naive_uload(NULL, "/bin/init");
     //context_kload(&pcb[1], (void *)hello_fun);
-    //context_uload(&pcb[1], "/bin/hello");
+    context_uload(&pcb[1], "/bin/hello");
     context_uload(&pcb[0], "/bin/pal");
     switch_boot_pcb();
 }
 
 _Context* schedule(_Context *prev) {
   current->cp = prev;
-  current = &pcb[0];
-  //current = (current == &pcb[0] ? &pcb[1] : &pcb[0]);
+  //current = &pcb[0];
+  current = (current == &pcb[0] ? &pcb[1] : &pcb[0]);
   return current->cp;
 }
