f87bdac3a52588ce980b6a7cf0a4623540314abe
tracer-ics2018 <tracer@njuics.org>
2018-12-12 12:40:37 +0800

    >  run 171860658 fermat Linux ics 4.9.0-8-amd64 #1 SMP Debian 4.9.130-2 (2018-10-27) x86_64 GNU/Linux  12:40:37 up 3 days,  3:43,  4 users,  load average: 0.05, 0.07, 0.02 1975c6f85fe784e56a5a79625164f374e5ce23

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -29,6 +29,7 @@ static uintptr_t loader(PCB *pcb, const char *filename) {
         //Log("Filesz = %x", filesz);
         va += PGSIZE;
     }
+    current->max_brk = (uintptr_t) va;
     //Log("Finish Allocating page");
 #endif
 
