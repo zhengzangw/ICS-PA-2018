a86c192fc25e985eaa69744e6052701b43e57771
tracer-ics2018 <tracer@njuics.org>
2018-12-12 23:18:36 +0800

    >  run 171860658 fermat Linux ics 4.9.0-8-amd64 #1 SMP Debian 4.9.130-2 (2018-10-27) x86_64 GNU/Linux  23:18:36 up 3 days, 12:56,  4 users,  load average: 0.22, 0.20, 0.13 84432e88edca6be22326c1cdce3a86e3eaadc581

--- nanos-lite/src/mm.c
+++ nanos-lite/src/mm.c
@@ -22,7 +22,7 @@ int mm_brk(uintptr_t new_brk) {
     _map(&current->as, (void *)current->max_brk, pa, 1);
     current->max_brk += PGSIZE;
   }
-  //Log("max_brk = %x", current->max_brk);
+  Log("max_brk = %x", current->max_brk);
   return 0;
 }
 
