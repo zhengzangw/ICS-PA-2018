1cb5250a386bc2a75a500644d14016d61fa1bc8f
tracer-ics2018 <tracer@njuics.org>
2018-12-13 20:57:05 +0800

    >  run 171860658 fermat Linux ics 4.9.0-8-amd64 #1 SMP Debian 4.9.130-2 (2018-10-27) x86_64 GNU/Linux  20:57:05 up  2:49,  2 users,  load average: 0.22, 0.23, 0.10 ac3e0376f902249b3b2b7b82ff5e91f09a2131d5

--- navy-apps/apps/init/init.cpp
+++ navy-apps/apps/init/init.cpp
@@ -67,6 +67,7 @@ static void display_menu(int n);
 
 int main(int argc, char *argv[], char *envp[]) {
   printf("qswll\n");
+  printf("qswll\n");
   if (!freopen("/dev/tty", "r", stdin) ||
       !freopen("/dev/tty", "w", stdout) ||
       !freopen("/dev/tty", "w", stderr)) {
