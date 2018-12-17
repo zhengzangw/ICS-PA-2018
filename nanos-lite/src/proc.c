56559634ed689097c70d065b2f17d9b8eb16673d
tracer-ics2018 <tracer@njuics.org>
2018-12-17 20:57:24 +0800

    >  run 171860658 fermat Linux ics 4.9.0-8-amd64 #1 SMP Debian 4.9.130-2 (2018-10-27) x86_64 GNU/Linux  20:57:24 up 2 days, 10:18,  3 users,  load average: 0.27, 0.24, 0.15 653bb656ff7a1f3e12e9ade65966f4067103ac18

--- nanos-lite/src/proc.c
+++ nanos-lite/src/proc.c
@@ -24,8 +24,8 @@ bool proc_hang[MAX_NR_PROC];
 uint32_t proc_cur,proc_cur_select;
 void init_proc() {
     //context_kload(&pcb[1], (void *)hello_fun);
-    context_uload(&pcb[0], "/bin/hello");  proc_hang[0] = false;
-    context_uload(&pcb[1], "/bin/bmptest"); proc_hang[1] = false;
+    context_uload(&pcb[1], "/bin/hello");  proc_hang[0] = false;
+    context_uload(&pcb[0], "/bin/bmptest"); proc_hang[1] = false;
     context_uload(&pcb[2], "/bin/bmptest"); proc_hang[2] = true;
     context_uload(&pcb[3], "/bin/init"); proc_hang[3] = true;
     proc_cur = 0;
