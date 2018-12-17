bd652371f02ae1e653026e7f86645bacd27436f3
tracer-ics2018 <tracer@njuics.org>
2018-12-17 19:22:33 +0800

    >  compile 171860658 fermat Linux ics 4.9.0-8-amd64 #1 SMP Debian 4.9.130-2 (2018-10-27) x86_64 GNU/Linux  19:22:33 up 2 days,  9:09,  2 users,  load average: 0.00, 0.02, 0.05 de5621b1c8aa655e41763213df1161351161b512

--- nemu/src/cpu/intr.c
+++ nemu/src/cpu/intr.c
@@ -11,8 +11,8 @@ void raise_intr(uint8_t NO, vaddr_t ret_addr) {
  rtlreg_t addr = cpu.idtr.addr + NO*8;
  assert(addr < cpu.idtr.addr+cpu.idtr.size);
 //Get Gate descriptor
- uint32_t gatedesc_lo = vaddr_read(addr, 2);
- uint32_t gatedesc_hi = vaddr_read(addr + 6, 2);
+ uint32_t gatedesc_lo = vaddr_read(addr, 4);
+ uint32_t gatedesc_hi = vaddr_read(addr + 4, 4);
 //Get offset
  assert(gatedesc_hi & 8000);
  uint32_t offset = (gatedesc_lo & 0xffff) + (gatedesc_hi & 0xffff0000);
