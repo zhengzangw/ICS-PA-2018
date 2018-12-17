9160903c86860494325fcadb5c96aa5f56a48a41
tracer-ics2018 <tracer@njuics.org>
2018-12-17 11:20:17 +0800

    >  run 171860658 fermat Linux ics 4.9.0-8-amd64 #1 SMP Debian 4.9.130-2 (2018-10-27) x86_64 GNU/Linux  11:20:17 up 2 days,  6:11,  2 users,  load average: 0.18, 0.19, 0.11 2cb89e2888550041e4ae1c7ca2ffbdffefa99e

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -10,7 +10,7 @@ static _Context* do_event(_Event e, _Context* c) {
             return schedule(c);
 			break;
         case _EVENT_IRQ_TIMER:
-            Log("receive _EVENT_IRQ_TIMER");
+            //Log("receive _EVENT_IRQ_TIMER");
             _yield();
             break;
     default: panic("Unhandled event ID = %d", e.event);
