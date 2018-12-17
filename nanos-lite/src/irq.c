ee35a043e4f085ebe51c77b0b9b43d134892c810
tracer-ics2018 <tracer@njuics.org>
2018-12-17 19:43:01 +0800

    >  run 171860658 fermat Linux ics 4.9.0-8-amd64 #1 SMP Debian 4.9.130-2 (2018-10-27) x86_64 GNU/Linux  19:43:01 up 2 days,  9:29,  2 users,  load average: 0.17, 0.12, 0.09 4cbb97144400713567d6f85e13517a7e14206ed3

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -1,6 +1,11 @@
 #include "common.h"
 #include "syscall.h"
 
+#define MAX_NR_PROC 4
+uint32_t time_slice[MAX_NR_PROC] = {10,1,0,0};
+uint32_t time_count, cur_proc;
+uint32_t total_proc = 2;
+
 static _Context* do_event(_Event e, _Context* c) {
   switch (e.event) {
 		case _EVENT_SYSCALL:
@@ -11,7 +16,12 @@ static _Context* do_event(_Event e, _Context* c) {
 			break;
         case _EVENT_IRQ_TIMER:
             Log("receive _EVENT_IRQ_TIMER");
-            _yield();
+            time_count++;
+            if (time_count == time_slice[cur_proc]){
+              time_count = 0;
+              cur_proc = (cur_proc + 1)%total_proc;
+              _yield();
+            }
             break;
     default: panic("Unhandled event ID = %d", e.event);
   }
